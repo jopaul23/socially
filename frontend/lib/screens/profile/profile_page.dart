12f4b7f65194fc4e396f9312af124344bdaeeb6f
jopaul23 <jopauljoy999@gmail.com>
2022-01-28 18:49:56 +0530

    logout addedd

--- frontend/lib/screens/profile/profile_page.dart
+++ frontend/lib/screens/profile/profile_page.dart
@@ -9,6 +9,7 @@ import 'package:internship_socialmedia/models/post_model.dart';
 import 'package:internship_socialmedia/models/user_model.dart';
 import 'package:internship_socialmedia/screens/profile/post_view_container.dart';
 import 'package:internship_socialmedia/screens/profile/profile_pictures.dart';
+import 'package:internship_socialmedia/screens/regitration/login/signin_page.dart';
 import 'package:internship_socialmedia/widget/bottom_navigation_bar.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -54,77 +55,108 @@ class _ProfilePageState extends State<ProfilePage> {
           padding: const EdgeInsets.all(defaultPadding),
           child: Column(
             children: [
-              Container(
-                height: 150,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    color: const Color(0xffF6F6F6),
-                    borderRadius: BorderRadius.circular(defaultPadding)),
-                child: Row(
-                  children: [
-                    const SizedBox(
-                      width: defaultPadding,
-                    ),
-                    isOwner
-                        ? GestureDetector(
-                            onTap: () {
-                              late OverlayEntry profileOverLay;
-                              profileOverLay = OverlayEntry(
-                                  builder: (context) => ProfileSelection(
-                                        userContrller: userContrller,
-                                        overlayEntry: profileOverLay,
-                                        user: user,
-                                      ));
-                              Overlay.of(context)?.insert(profileOverLay);
-                            },
-                            child: Stack(
-                              alignment: Alignment.bottomRight,
-                              children: [
-                                CircleAvatar(
-                                  backgroundImage: NetworkImage(user.profile),
-                                  radius: 35,
-                                ),
-                                Container(
-                                  padding: EdgeInsets.all(5),
-                                  decoration: BoxDecoration(
-                                      color: primaryBlue,
-                                      borderRadius: BorderRadius.circular(20)),
-                                  child: const Icon(
-                                    Icons.add_a_photo_rounded,
-                                    size: 20,
-                                    color: white,
-                                  ),
-                                )
-                              ],
-                            ),
-                          )
-                        : CircleAvatar(
-                            backgroundImage: NetworkImage(user.profile),
-                            radius: 35,
-                          ),
-                    const SizedBox(
-                      width: defaultPadding,
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
+              Stack(
+                children: [
+                  Container(
+                    height: 150,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: const Color(0xffF6F6F6),
+                        borderRadius: BorderRadius.circular(defaultPadding)),
+                    child: Row(
                       children: [
                         const SizedBox(
-                          height: 10,
+                          width: defaultPadding,
                         ),
-                        Text(
-                          user.name,
-                          style: TextStyle(
-                            fontSize: 18.sp,
-                            fontWeight: FontWeight.w600,
-                            color: primaryBlue,
-                          ),
+                        isOwner
+                            ? GestureDetector(
+                                onTap: () {
+                                  late OverlayEntry profileOverLay;
+                                  profileOverLay = OverlayEntry(
+                                      builder: (context) => ProfileSelection(
+                                            userContrller: userContrller,
+                                            overlayEntry: profileOverLay,
+                                            user: user,
+                                          ));
+                                  Overlay.of(context)?.insert(profileOverLay);
+                                },
+                                child: Stack(
+                                  alignment: Alignment.bottomRight,
+                                  children: [
+                                    CircleAvatar(
+                                      backgroundImage:
+                                          NetworkImage(user.profile),
+                                      radius: 35,
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.all(5),
+                                      decoration: BoxDecoration(
+                                          color: primaryBlue,
+                                          borderRadius:
+                                              BorderRadius.circular(20)),
+                                      child: const Icon(
+                                        Icons.add_a_photo_rounded,
+                                        size: 20,
+                                        color: white,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              )
+                            : CircleAvatar(
+                                backgroundImage: NetworkImage(user.profile),
+                                radius: 35,
+                              ),
+                        const SizedBox(
+                          width: defaultPadding,
                         ),
-                        Text(user.email)
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              user.name,
+                              style: TextStyle(
+                                fontSize: 18.sp,
+                                fontWeight: FontWeight.w600,
+                                color: primaryBlue,
+                              ),
+                            ),
+                            Text(user.email)
+                          ],
+                        ),
                       ],
                     ),
-                  ],
-                ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: GestureDetector(
+                      onTap: () async {
+                        SharedPreferences prefs =
+                            await SharedPreferences.getInstance();
+                        prefs.remove("auth-token");
+                        prefs.remove("user_name");
+                        prefs.remove("email");
+
+                        prefs.remove("user_id");
+                        Get.off(() => SigninPage());
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            color: primaryBlue,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Text(
+                          "logout",
+                          style: TextStyle(color: white, fontSize: 12.sp),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
               ),
               const SizedBox(
                 height: 10,
